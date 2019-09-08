e8eabfc4887cbfe114531134d2112491ac684a06
naishadh <voranaishadh@gmail.com>
2019-09-09 00:50:58 +0530

    Added commands ls and ls with color

--- client.py
+++ client.py
@@ -2,15 +2,41 @@
 from socket import *
 import sys
 import os
+import json
+os.system('color')
 
 global control_port
 
 class State:
-    def __init__(self, clientSocket, serverName):
+    def __init__(self, clientSocket, dataSocket, serverName):
         self.cwd = os.getcwd()
         self.folder = os.path.basename(self.cwd)
         self.control = clientSocket
+        self.data = dataSocket
         self.server = serverName
+
+
+def rls(state):
+    state.control.send("ls".encode('ascii'))
+    dirs = state.data.recv(1024).decode('ascii')
+    dirlist = json.loads(dirs)
+    for l in dirlist:
+        if '.' in l:
+            print(l, end='    ')
+        else:
+            CRED = '\033[91m'
+            CEND = '\033[0m'
+            print(CRED + l + CEND, end='    ')
+    print()
+
+def lls(state):
+    dirlist = os.listdir(state.cwd)
+    for l in dirlist:
+        print(l, end='    ')
+    print()
+
+def cd(state):
+    pass
 
 if __name__ == '__main__':
     global control_port
@@ -20,14 +46,26 @@ if __name__ == '__main__':
     #control_port = int(sys.argv[2])
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName, control_port))
-    state = State(clientSocket, serverName)
+    data_port = int(clientSocket.recv(1024).decode('ascii'))
+    dataSocket = socket(AF_INET, SOCK_STREAM)
+    dataSocket.connect((serverName, data_port))
+    state = State(clientSocket, dataSocket, serverName)
+    print('The control port is {} and the data port is {}'.format(control_port, data_port))
 
     while True:
-        sentence = input('ftp> ')
-        state.control.send(sentence.encode('ascii'))
-        if(sentence == 'bye'):
+        command = input('ftp> ')
+        #SWITCH BASED ON COMMAND
+        if(command == "bye"):
             break
-        text = state.control.recv(1024)
-        print(text.decode('ascii'))
+        elif(command == "ls"):
+            rls(state)
+        elif(command == "!ls"):
+            lls(state)
+        elif(command[0:3] == "cd "):
+            state.command = command
+            cd(state)
+        else:
+            print("Incorrect command!")
+
 
     state.control.close()
--- server.py
+++ server.py
@@ -3,25 +3,38 @@ from socket import *
 from threading import Thread
 import sys
 import os
+import json
 
-global control_port
-
+global control_port, thread_list
 class State:
-    def __init__(self, connectionSocket, addr):
+    def __init__(self, connectionSocket, addr, count):
+        global control_port
         self.cwd = os.getcwd()
         self.folder = os.path.basename(self.cwd)
         self.control = connectionSocket
-        self.addr = addr
+        self.control_addr = addr
+        self.data_port = control_port + count
+        self.data_socket = socket(AF_INET, SOCK_STREAM)
+        self.data_socket.bind(('', self.data_port))
         #ALSO DEFINE SELF.DATA
+
+def threads(control_socket):
+    global thread_list
+    while len(thread_list) == 0:
+        continue
+    if(len(thread_list) == 0):
+        control_socket.close()
+        sys.exit(0)
 
 def ls(state):
     dirlist = os.listdir(state.cwd)
-    for l in dirlist:
-        print(l)
-    state.control.send(state.command.encode('ascii'))
+    state.data.send(json.dumps(dirlist).encode('ascii'))
 
 def connection(state):
     print("New connection to client {}".format(addr))
+    state.control.send(str(state.data_port).encode('ascii'))
+    state.data_socket.listen(1)
+    state.data, state.data_addr = state.data_socket.accept()
     while True:
         command = state.control.recv(1024).decode('ascii')
         #SWITCH BASED ON command
@@ -29,19 +42,26 @@ def connection(state):
             state.command = "ls"
             ls(state)
 
+    state.data.close()
     state.control.close()
 
 if __name__ == '__main__':
-    global control_port
+    global control_port, thread_list
     control_port = int(sys.argv[1])
     control_socket = socket(AF_INET, SOCK_STREAM)
     control_socket.bind(('', control_port))
     control_socket.listen(10)
+    count = 1
+    thread_list = []
+    t = Thread(target=threads, args=(control_socket, ))
+    t.start()
 
     while True:
         connectionSocket, addr = control_socket.accept()
-        state = State(connectionSocket, addr)
+        state = State(connectionSocket, addr, count)
         t = Thread(target=connection, args=(state,))
         t.start()
+        count += 1
+        thread_list.append(t)
 
     control_socket.close()
